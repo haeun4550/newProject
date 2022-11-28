b602d8497ce3c87993389096fb4b0f796d1f3d18
haeun4550 <118503386+haeun4550@users.noreply.github.com>
2022-11-29 00:24:55 +0900

    Update login.js

--- login.js
+++ login.js
@@ -15,8 +15,4 @@ window.onload = function(){
    input_id = document.getElementById("id");
    input_pw = document.getElementById("pw");
     div= document.getElementById("loginForm");
-    
-    $("#id").on("change keyup paste", function(){
-	alert("change");
-})
 }
